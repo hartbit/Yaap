c78195bea903fdb3d7d492bccb3d5d7c1acd4ebd
David Hart <david@atipik.ch>
2019-12-23 11:24:41 +0100

    Add a binary target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -8,7 +8,9 @@ let package = Package(
     products: [
         .library(
             name: "Yaap",
-            targets: ["Yaap"])
+            targets: ["Yaap"]),
+        .library(
+            name: "YaapBinary", targets: ["YaapBinary"]),
     ],
     targets: [
         .target(
@@ -19,7 +21,11 @@ let package = Package(
             dependencies: ["Yaap"]),
         .testTarget(
             name: "YaapTests",
-            dependencies: ["Yaap"])
+            dependencies: ["Yaap"]),
+        ._binaryTarget(
+            name: "YaapBinary",
+            url: "https://user.fm/files/v2-9eeb943dc13e8f38f72997d1e24db5c5/YaapBinary.zip",
+            checksum: "c03ee54701fc2e7f6e4eebbec8091c17a3904fa3394c23d37da2325c29b06d5c"),
     ],
     swiftLanguageVersions: [.v5]
 )
---
+++ YaapBinary/YaapBinary.h
@@ -0,0 +1,19 @@
+//
+//  YaapBinary.h
+//  YaapBinary
+//
+//  Created by David Hart on 23.12.19.
+//  Copyright © 2019 David Hart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YaapBinary.
+FOUNDATION_EXPORT double YaapBinaryVersionNumber;
+
+//! Project version string for YaapBinary.
+FOUNDATION_EXPORT const unsigned char YaapBinaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YaapBinary/PublicHeader.h>
+
+
